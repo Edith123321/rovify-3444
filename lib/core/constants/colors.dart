a12cadbb762faf05915b2a47c522690196f9054a
Dalington6 <d.tunyinko@alustudent.com>
2025-07-14 18:55:08 +0200

    Define recurring/reusable app colors

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,20 @@
+// lib/core/constants/colors.dart
+
+// Import packages/modules
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color splashBackground1 = Color(0xFFFF5900); // Background color for splash screen 1
+  static const Color splashBackground2 = Color(0xFFC281FF); // Background color for splash screen 2
+  static const Color splashBackground3 = Color(0xFF3329CF); // Background color for splash screen 3
+  static const Color splashBackground4 = Color(0xFF18E299); // Background color for splash screen 4
+
+  static const Color primaryBackground = Color(0xFFFFFFFF); // General background color for all other screens
+  static const Color activeTranslucentBlack = Color(0xB5000000); // Color for active dot
+  static const Color inactiveTranslucentBlack = Color(0x47000000); // Color for inactive dots
+
+  static const Color splash1 = Color(0xFF551E00); // For texts and button on spalsh screen 1
+  static const Color splash2 = Color(0xFF340065); // For texts and button on spalsh screen 2
+  static const Color splash3 = Color(0xFF050059); // For texts and button on spalsh screen 3
+  static const Color splash4 = Color(0xFF00462D); // For texts and button on spalsh screen 4
+}
